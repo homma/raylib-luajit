85b6cc8e0f319c8fe7958ea1f3a454c6612e1709
Daisuke Homma <maada@Mac-mini.elecom>
2023-09-18 14:14:40 +0900

    committed on 2023/09/18 14:14.

--- raylib.lua
+++ raylib.lua
@@ -1,10 +1,22 @@
 local ffi = require 'ffi'
 
+local basename = function(path)
+  local p = {}
+  for w in string.gmatch(path, '[^/]+') do
+    table.insert(p, w)
+  end
+  table.remove(p)
+  local base = table.concat(p, '/')
+  return base
+end
+
+local script_dir = basename(debug.getinfo(1).short_src) .. '/'
+
 local M = {}
 
 local init_ffi = function()
   -- initialize ffi
-  local f = io.open('ext/raylib.cdef', 'r')
+  local f = io.open(script_dir .. 'ext/raylib.cdef', 'r')
   local cdefs = f:read 'a'
   ffi.cdef(cdefs)
 
